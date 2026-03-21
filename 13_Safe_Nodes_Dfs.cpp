ee77a8205e990fb73f2b9e469fcccdeb39f61bf9
shripad69 <shripadalgude@gmail.com>
2026-03-21 19:37:04 +0530

    safe nodes

--- 13_Safe_Nodes_Dfs.cpp
+++ 13_Safe_Nodes_Dfs.cpp
@@ -6,70 +6,53 @@ using namespace std;
 
 class Solution {
 public:
+    
 
-    bool dfs(int node, vector<int>& vis, vector<int>& path,
-             vector<vector<int>>& graph, vector<int>& unsafe) {
+    bool dfs (int node, vector <int>& vis, vector <int>&path, vector <int>& unsafe, vector <vector <int>>& graph) {
 
+        int n = graph.size();
         vis[node] = 1;
         path[node] = 1;
 
-        for (int nei : graph[node]) {
 
-            // Case 1: not visited → explore
-            // ek pn unsafe ghavli tr current node la unsafe kr and true return kr vrti
-            if (!vis[nei]) {
-                if (dfs(nei, vis, path, graph, unsafe)) {
-                    unsafe[node] = 1;   // current becomes unsafe
+        for (auto i : graph[node]) {
+
+            if (!vis[i]) {
+                if (dfs(i, vis, path, unsafe, graph)){
+                    unsafe[node] = 1;
                     path[node] = 0;
                     return true;
                 }
+                
             }
-
-            // Case 2: back-edge → cycle
-            // cycle ghavli tr current la unsafe kra ani true return kr vr
-            else if (path[nei]) {
-                unsafe[node] = 1;   // current in cycle → unsafe
-                path[node] = 0;
-                return true;
-            }
-
-
-            // imp ---------->
-            
-            // Case 3: already known unsafe
-            // jr koni already unsafe ahe and tula connected ahe tr mg tu pn unsafe
-            else if (unsafe[nei]) {
-                unsafe[node] = 1;   // leads to unsafe → unsafe
+            else if (path[i] || unsafe[i]) {
+                unsafe[node] = 1;
                 path[node] = 0;
                 return true;
             }
         }
 
-        path[node] = 0;  // backtrack
-        return false;    // safe node
+        path[node] = 0;
+        return false;
     }
 
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
 
         int n = graph.size();
-
-        vector<int> vis(n, 0);
-        vector<int> path(n, 0);
-        vector<int> unsafe(n, 0);  // 1 = unsafe, 0 = safe
+        vector <int> vis(n, 0), path(n, 0), unsafe(n, 0);
 
         for (int i = 0; i < n; i++) {
+
             if (!vis[i]) {
-                dfs(i, vis, path, graph, unsafe);
+                dfs(i, vis, path, unsafe, graph);
             }
         }
 
-        vector<int> ans;
+        vector <int> safenodes;
         for (int i = 0; i < n; i++) {
-            if (!unsafe[i]) {
-                ans.push_back(i);
-            }
+            if (!unsafe[i]) safenodes.push_back(i);
         }
-
-        return ans;
+        return safenodes;
+        
     }
 };
