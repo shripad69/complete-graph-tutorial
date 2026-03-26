24ccdf5014ddf252b368c538f58e90aa75aceb3a
shripad69 <shripadalgude@gmail.com>
2026-03-26 21:36:43 +0530

    ways

---
+++ 26_Number_Of_Ways_Destination.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#include <queue>
+using namespace std;
+
+
+
+class Solution {
+public:
+    int countPaths(int n, vector<vector<int>>& roads) {
+        
+        vector <vector < pair <int, int>>> adj(n);
+        for (int i = 0; i < roads.size(); i++) {
+            int node1 = roads[i][0];
+            int node2 = roads[i][1];
+            int dist = roads[i][2];
+
+            adj[node1].push_back({dist, node2});
+            adj[node2].push_back({dist, node1});
+        }
+
+        vector <int> dist (n,  INT_MAX), ways(n, 0);
+        dist[0] = 0;
+        dist[0] = 1;
+
+        priority_queue<pair <int, int>> pq;
+        pq.push({0, 0});
+
+        while (!pq.empty()) {
+
+            int node = pq.top().second;
+            int weight = pq.top().first;
+            pq.pop();
+
+            for (auto i : adj[node]) {
+                if (dist[i.second] > weight + i.first) {
+                    dist[i.second] = weight + i.first;
+                    ways[i.second] = ways[node];
+                    pq.push({dist[i.second], i.second});
+                }
+                else if (dist[i.second] > weight + i.first) {
+                    ways[i.second] += ways[node];
+                    pq.push({dist[i.second], i.second});
+                }
+            }
+        }
+
+
+        return ways[n - 1];
+    }
+};
